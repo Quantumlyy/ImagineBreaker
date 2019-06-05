35c107bbee77ede4c5770bc17e0868755c12ac14
QuantumlyTangled <ethoarx@gmail.com>
2019-06-05 21:38:46 +0200

    Basic fortnite user fetching

--- ImagineBreaker.Games/Fortnite/FortniteClient.cs
+++ ImagineBreaker.Games/Fortnite/FortniteClient.cs
@@ -1,5 +1,8 @@
 using System.Net.Http;
+using System.Threading.Tasks;
+using ImagineBreaker.Games.Fortnite.Responses;
 using ImagineBreaker.Util;
+using Newtonsoft.Json;
 
 namespace ImagineBreaker.Games.Fortnite
 {
@@ -7,7 +10,7 @@ namespace ImagineBreaker.Games.Fortnite
     {
         public static FortniteClient Client = new FortniteClient();
         
-        private const string ApiUrl = "https://fortnite-api.theapinetwork.com/";
+        private const string ApiUrl = "https://fortnite-api.theapinetwork.com";
         private readonly HttpClient _httpClient = ImagineBreakerSingletons.HttpClient;
         private readonly string _token;
 
@@ -15,6 +18,12 @@ namespace ImagineBreaker.Games.Fortnite
         {
             _token = "9c72c3fffe74a5e6c1a53d6f96d98352";
             _httpClient.DefaultRequestHeaders.Add("Authorization", _token);
+        }
+
+        public async Task<Users> GetUserAsync(string user)
+        {
+            var response = await _httpClient.GetAsync($"{ApiUrl}/users/id?username={user.ToLower()}");
+            return JsonConvert.DeserializeObject<Users>(response.ToString());
         }
     }
 }
